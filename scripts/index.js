2a28b973b83c08c2bf9f076ff501ec5fe3f295dc
Davidvboxt <davidvb00@gmail.com>
2022-12-23 10:06:30 +0100

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,66 +1,20 @@
-jQuery( document ).ready(function( $ ) {
+// Slider
 
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
-	"use strict";
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
 
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-		$('.owl-carousel').owlCarousel({
-		    items:4,
-		    lazyLoad:true,
-		    loop:true,
-		    dots:true,
-		    margin:20,
-		    responsiveClass:true,
-			    responsive:{
-			        0:{
-			            items:1,
-			        },
-			        600:{
-			            items:2,
-			        },
-			        1000:{
-			            items:4,
-			        }
-			    }
-		});
-
-		/* activate jquery isotope */
-		  var $container = $('.posts').isotope({
-		    itemSelector : '.item',
-		    isFitWidth: true
-		  });
-
-		  $(window).smartresize(function(){
-		    $container.isotope({
-		      columnWidth: '.col-sm-3'
-		    });
-		  });
-		  
-		  $container.isotope({ filter: '*' });
-
-		    // filter items on button click
-		  $('#filters').on( 'click', 'button', function() {
-		    var filterValue = $(this).attr('data-filter');
-		    $container.isotope({ filter: filterValue });
-		});
-
-
-		$('#carousel').flexslider({
-		    animation: "slide",
-		    controlNav: false,
-		    animationLoop: false,
-		    slideshow: false,
-		    itemWidth: 210,
-		    itemMargin: 5,
-		    asNavFor: '#slider'
-		});
-		 
-		$('#slider').flexslider({
-		    animation: "slide",
-		    controlNav: false,
-		    animationLoop: false,
-		    slideshow: false,
-		    sync: "#carousel"
-		});
- 
-});
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
